14385e79e59926b2a665f1a41bc7992f65ca1bca
Fouzia33 <fouzia.hollande@gmail.com>
2021-12-28 14:13:36 +0100

    component knowledges ok

--- src/components/knowledges/Hobbies.js
+++ src/components/knowledges/Hobbies.js
@@ -3,6 +3,35 @@ import React from 'react';
 const Hobbies = () => {
     return (
         <div className='hobbies'>
+            <h3>Intérêts</h3>
+            <ul>
+
+                <li className='hobby'>
+                    <i className='fas fa-hiking'></i>
+                    <span> Randonnées </span>
+                </li>
+
+                <li className='hobby'>
+                    <i className='fas fa-spa'></i>
+                    <span> Yoga </span>
+                </li>
+
+                <li className='hobby'>
+                    <i className='fas fa-plane'></i>
+                    <span> Voyage </span>
+                </li>
+
+                <li className='hobby'>
+                    <i className='fas fa-seedling'></i>
+                    <span> Jardinage </span>
+                </li>
+
+                <li className='hobby'>
+                    <i className='fas fa-film'></i>
+                    <span> Séries </span>
+                </li>
+
+            </ul>
         
         </div>
     );
